d2aadb5812f17377114da6b36b4c393476237580
denis <ridchenkodenis@gmail.com>
2022-02-07 13:35:05 +0200

    first commit

---
+++ js/common.js
@@ -0,0 +1,33 @@
+$(function() {
+
+	//fancybox
+    Fancybox.bind("[data-fancybox]", {
+		infinite: true,
+		dragToClose: false,
+		trapFocus: false,
+		placeFocusBack: false,
+		hideScrollbar: false,
+  });
+
+
+		const mainSlider = new Swiper('.main-slider', {
+
+  loop: true,
+	speed: 700,
+	grabCursor: true,
+	observer: true,
+
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.main-button--next',
+    prevEl: '.main-button--prev',
+  },
+	keyboard: {
+        enabled: true,
+        onlyInViewport: true,
+	},
+
+});
+
+});
