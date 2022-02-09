16c89f2fe6f96146e0bb27b23a36811ecdbfaf52
denis <ridchenkodenis@gmail.com>
2022-02-09 02:39:07 +0200

    edit: added responsive for main-page

--- js/common.js
+++ js/common.js
@@ -30,4 +30,36 @@ $(function() {
 
 });
 
+
+	var nav = $('.header');
+ 
+	$(window).scroll(function () {
+		if ($(this).scrollTop() > 136) {
+			nav.addClass("header_scroll");
+		} else {
+			nav.removeClass("header_scroll");
+		}
+	});
+
+let menuButton = document.querySelector('.header-burger');
+let hiddenMenu = document.querySelector('.hidden-menu');
+let hiddenMenuNav = document.querySelectorAll('.hidden-menu__nav a');
+
+menuButton.addEventListener('click', function() {
+	menuButton.classList.toggle('_active');
+	hiddenMenu.classList.toggle('_visible');
+});
+
+
+hiddenMenuNav.forEach((item, i) => {
+	item.addEventListener('click', () => {
+			menuButton.classList.remove('_active');
+			hiddenMenu.classList.remove('_visible');
+	});
+});
+
+
+
+
+
 });
